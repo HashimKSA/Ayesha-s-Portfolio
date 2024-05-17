3c04fb65cd9c26da3cabacd50d3c000eeefb538b
HashimKSA <168968624+HashimKSA@users.noreply.github.com>
2024-05-17 15:28:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+<script>
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classlist.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+}
+
+
+</script>
